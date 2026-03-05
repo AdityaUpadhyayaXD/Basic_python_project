028ad784e14c09de231fe026a28005f806387ebd
AdityaUpadhyayaXD <adityaupadhyaya23@gmail.com>
2026-03-06 01:57:03 +0530

    control flow and use of if-else statement inpython

---
+++ cumulative_range.py
@@ -0,0 +1,7 @@
+previous_num = 0
+sum = 0
+for num in range(0,10):
+    sum = num + previous_num
+    print(f" the current num is{num} the previous num is {previous_num} and sum is {sum}")
+    previous_num = num
+
---
+++ even_slicing.py
@@ -0,0 +1,11 @@
+string = "agjberjgbergb"
+string_len = len(string)
+
+for i in range(0,string_len):
+    if i % 2 == 0:
+        print(string[i])
+    else:
+        pass
+
+
+
