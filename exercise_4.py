9d1f90d89ddb499a1f04e7b74340971b722324a4
AdityaUpadhyayaXD <adityaupadhyaya23@gmail.com>
2026-03-16 16:30:56 +0530

    control flow and use of if-else statement inpython

---
+++ exercise_4.py
@@ -0,0 +1,8 @@
+def remove_char(string, input_char):
+    str1 = string
+    number = input_char + 1
+    return str1.slice(0,number)
+
+
+
+print(remove_char("aditya", 3))
---
+++ practiceQuestion.py
@@ -0,0 +1,33 @@
+# movies = []
+#
+# movies.append(input("Enter movie name: "))
+# movies.append(input("Enter movie name: "))
+# print(movies)
+
+# list = [1,2,3,2,1,7]
+#
+# list2 = list.copy()
+# # print(list2)
+# list2.reverse()
+#
+# if list == list2:
+#     print("palandrome")
+#declare a list
+# my_list =["hello", 23, 23.4,True,None]
+# print(my_list[0])
+# print(my_list[2])
+# print(my_list[4])
+# my_detail  = ["aditya",19,177,"single","home"]
+# print(my_detail[0])
+# ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+# ages.sort()
+# print(ages)
+# maximum_number= max(ages)
+# minimum_number = min(ages)
+# print(maximum_number)
+# print(minimum_number)
+def generate_full_name(f_name,l_name):
+    full_name = f_name + " " + l_name
+    return full_name
+print(generate_full_name("Aditya","Upadhyaya"))
+
